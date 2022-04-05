7be097fd55f0de9b10282150de70f6e8692c19ea
luzmar7 <luzmar7.luz@gmail.com>
2022-04-05 13:43:57 -0400

    practicando useState

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const useCounter = ( initialState = 10 ) => {
+  const [state, setState] = useState(initialState); // 10
+  const increment = ( factor = 1 ) => {
+    setState( state + factor )
+  }
+  const decrement = ( factor = 1 ) => {
+    setState( state - factor )
+  }
+  const reset = () => {
+    setState( initialState )
+  }
+
+  return { 
+    state,
+    increment,
+    decrement,
+    reset
+  }
+}
+
+export default useCounter
